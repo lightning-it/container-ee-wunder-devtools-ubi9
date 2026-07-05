7857a57940b2f56d3f598c11e2a59e5218a11089
litroc <76040632+litroc@users.noreply.github.com>
2026-07-05 04:16:12 +0000

    chore: sync shared assets from lightning-it/shared-assets-lit

--- scripts/devtools-container-ci.sh
+++ scripts/devtools-container-ci.sh
@@ -72,11 +72,6 @@ run_shellcheck() {
 }
 
 run_actionlint() {
-  if command -v actionlint >/dev/null 2>&1; then
-    actionlint
-    return
-  fi
-
   require_docker
   set_nested_workspace_args
   docker run --rm \
@@ -153,7 +148,6 @@ run_contract_tests() {
         ansible-lint --version
         pre-commit --version
         gh --version
-        actionlint --version
         docker --version
         antsibull-changelog --version
       '
